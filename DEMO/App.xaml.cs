0d2b036748d487a0436a5c0c4b4fb7484b4aeeb4
RAQUEL <RAQUEL@LAPTOP-TMEAL0V3>
2023-02-24 20:28:25 -0600

    Add project files.

---
+++ DEMO/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DEMO.views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DEMO
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new views.Page1());
+            Page.BindingContext= Page1
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DEMO/views/Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DEMO.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+       var  Page = new views.Page2();
+            
+        private async void txtboton1_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("SALUDOS:", txtNombre1.Text + " " +txtapellido1.Text, "ok");
+        }
+    }
+}
